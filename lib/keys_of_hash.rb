18ffbf7b993ae206a9032519b6cd07f9572ee761
Abdul <awsayed@alaska.edu>
2019-05-05 05:18:40 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,28 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+
+  def keys_of(*values)
+		print self
+		puts ""
+		print self.keys
+		puts ""
+		print self.values
+		puts ""
+		print values
+		puts ""
+
+		keys = []
+		self.each { |k, v|
+
+			values.each { |value|
+				if (v == value)
+					puts "#{value} matched for #{k}"
+					keys << k
+				end 
+			}
+
+		}
+		print keys
+		return keys
   end
-end
+
+end 
